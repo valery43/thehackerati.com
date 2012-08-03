1afd4d031c735b68331ee2f85af98057cfba225c
Valeri Nakonechny <valery43@usa.com>
2012-08-03 01:10:33 -0300

    Update for sitemap generation

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
-from django.contrib.sitemaps import GenericSitemap
-from django.contrib.sites.models import Site
+from sitemaps import DirectToTemplateSitemap, PeopleSitemap
+
 from django.conf.urls.defaults import *
 import settings
 
@@ -7,15 +7,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 handler500 = 'djangotoolbox.errorviews.server_error'
-
-info_dict = {
-    'queryset': Site.objects.all(),
-    'date_field': 'pub_date',
-}
-
-sitemaps = {
-    'thehackerati': GenericSitemap(info_dict, priority=0.6, changefreq='daily'),
-}
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
@@ -52,6 +43,12 @@ urlpatterns += patterns('',
     url(r'^people/', include('thehackerati_team.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+sitemaps = {
+      'pages' : DirectToTemplateSitemap(urlpatterns),
+      'people' : PeopleSitemap,
+}
 
 urlpatterns += patterns('',
     (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
